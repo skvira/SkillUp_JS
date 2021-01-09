1b11e55f005529cd32ad6e6498bb8782ddead988
Olena Lobachova <skvira0803@gmail.com>
2021-01-09 15:08:23 +0200

    v1 Homework-01-add

--- task-01.js
+++
@@ -1,7 +0,0 @@
-let product_name = 'Генератор защитного поля';
-let price = 1000;
-
-console.log(`Выбран « ${product_name}», цена за штуку ${price} кредитов`);
-
-price = 2000;
-console.log(`Выбран « ${product_name}», цена за штуку ${price} кредитов`);
--- task-02.js
+++
@@ -1,9 +0,0 @@
-const total = 100;
-const ordered = 50;
-
-let isEnough = total >= ordered;
-
-let message = isEnough ? 'Заказ оформлен, с вами свяжется менеджер' : 'На складе недостаточно твоаров!'
-
-//console.log = (message)
-alert(message)
