98db8d3a9583b3dc537981874f52342a5a55f297
Olena Lobachova <skvira0803@gmail.com>
2021-01-18 22:19:15 +0200

    after rename + hw02-add task3

--- task-03-add.js
+++ task-03-add.js
@@ -1,3 +1,15 @@
 // Написати ф-ю яка приймає 2 параметра (розміри в ремах - "2rem") і
 // велечину базового шрифта ("20px") і вертає конвертований розмір в
 // пікселях
+
+function convert_rem_to_px (rem, pxBase) {
+    
+    let remNum = Number(rem.replace('rem',''));
+    let pxBaseNum = Number(pxBase.replace('px',''));
+    let outPx = remNum*pxBaseNum + 'px';
+    console.log(outPx);
+    
+    return outPx;
+}
+
+convert_rem_to_px('30rem', '50px');
